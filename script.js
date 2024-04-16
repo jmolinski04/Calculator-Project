ac205d62a009b9733cdf672e7edeaf7f300c7113
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-16 17:07:01 +0200

    The app is able to show 9 numbers inputted to the div onclick

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const errorMessage = document.querySelector(".error-msg");
+const result = document.querySelector(".result");
+const operatorButtons = document.querySelectorAll(".btn-operator");
+const numberButtons = document.querySelectorAll(".btn-number");
+const clearButton = document.querySelector(".clear-btn");
+
 const addNumbers = (a, b) => a + b;
 const subtractNumbers = (a, b) => a - b;
 const multiplyNumbers = (a, b) => a * b;
@@ -12,10 +17,25 @@ const operate = (num1, operator, num2) => {
     return num2 === 0
       ? (errorMessage.innerText = "Can't divide by 0")
       : divideNumbers(num1, num2);
-
-  //   return (errorMessage.innerText = "Can't divide by 0");
-  // } else {
-  //   return divideNumbers(num1, num2);
-  // }
 };
-operate(4, "/", 2);
+
+const displayNumbers = () => {
+  const numberLimit = 9;
+  let count = 0;
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (count < numberLimit) {
+        result.innerHTML += button.value;
+        count++;
+      }
+    });
+  });
+};
+displayNumbers();
+
+clearButton.addEventListener("click", () => {
+  result.textContent = "";
+  displayNumbers();
+});
+
+// result.textContent = operate(4, "/", 3);
