f1f83b34aa6a6609cac30f01bd4b8df5f3032c9c
Jakub Molinski <jakubmolinski9@gmail.com>
2024-04-19 16:22:04 +0200

    Now in the app user can input a number, an operator and second number, equals button is not working yet

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const result = document.querySelector(".result");
 const operatorButtons = document.querySelectorAll(".btn-operator");
 const numberButtons = document.querySelectorAll(".btn-number");
 const clearButton = document.querySelector(".clear-btn");
+const equalsButton = document.querySelector(".btn-equals");
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+result.value = 0;
 
 const addNumbers = (a, b) => a + b;
 const subtractNumbers = (a, b) => a - b;
@@ -19,23 +24,48 @@ const operate = (num1, operator, num2) => {
       : divideNumbers(num1, num2);
 };
 
-const displayNumbers = () => {
-  const numberLimit = 9;
-  let count = 0;
-  numberButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (count < numberLimit) {
-        result.innerHTML += button.value;
-        count++;
-      }
-    });
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", () => {
+    // if there is no operator that means that i'm saving first number
+    if (operator === "") {
+      firstNum += numberButton.value;
+      result.value = firstNum;
+      // else i'm saving second number because if there is an operator it means that i have to operate on these two numbers
+    } else {
+      secondNum += numberButton.value;
+      result.value = secondNum;
+    }
   });
-};
-displayNumbers();
-
-clearButton.addEventListener("click", () => {
-  result.textContent = "";
-  displayNumbers();
 });
 
-// result.textContent = operate(4, "/", 3);
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener("click", (e) => {
+    if (e.target.value !== equalsButton.value) {
+      operator = e.target.value;
+      result.value = operator;
+    } else {
+      switch (operator) {
+        case "+":
+          result.value = operate(firstNum, operator, secondNum);
+          break;
+        case "-":
+          console.log(operate(firstNum, "-", secondNum));
+          break;
+        case "*":
+          console.log(operate(firstNum, "*", secondNum));
+          break;
+
+        case "/":
+          console.log(operate(firstNum, "/", secondNum));
+          break;
+        default:
+          break;
+      }
+    }
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  result.innerHTML = "";
+  location.reload();
+});
